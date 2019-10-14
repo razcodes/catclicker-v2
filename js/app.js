9daa315ffbf2e5549f94fe16824a8e957a59a815
Raz Rimon <razrimon27@gmail.com>
2019-10-14 19:02:24 +0300

    Added admin mode

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+// Global variables
+let nameInput = document.getElementById('name-input');
+let urlInput = document.getElementById('url-input');
+let clicksInput = document.getElementById('clicks-input');
+
 var model = {
 	currentCat: null,
 	cats: [
@@ -40,6 +45,38 @@ var model = {
 };
 
 var octopus = {
+	changeAdminFields: () => {
+		let currentCat = model.currentCat;
+
+		nameInput.value = currentCat.name;
+		urlInput.value = currentCat.url;
+		clicksInput.value = currentCat.clickCount;
+	},
+	clearAdminFields: () => {
+		let inputs = document.querySelectorAll('input');
+		let inputsArray = Array.prototype.slice.call(inputs);
+		inputsArray.forEach((input) => {
+			input.value = "";
+		});
+	},
+	changeCurrentCatName: (name) => {
+		let currentCat = document.getElementById(model.currentCat.name);
+		currentCat.textContent = name;
+		model.currentCat.name = name;
+		currentCat.setAttribute('id', name);
+	},
+	changeCurrentCatURL: (url) => {
+		model.currentCat.url = url;
+	},
+	changeCurrentCatClicks: (clicks) => {
+		model.currentCat.clickCount = clicks;
+	},
+	toggleVisibility: (element) => {
+		element.classList.toggle('hide');
+	},
+	hideAdminPanel: () => {
+		document.getElementById('admin-panel').classList.add('hide');
+	},
 	getCats: () => {
 		return model.cats;
 	},
@@ -57,6 +94,54 @@ var octopus = {
 		model.currentCat = model.cats[0];
 		listView.init();
 		detailView.init();
+		adminView.init();
+	}
+};
+
+var adminView = {
+	init: () => {
+		this.adminButton = document.getElementById('admin-button');
+		this.adminPanel = document.getElementById('admin-panel');
+		this.cancelButton = document.getElementById('cancel-button');
+		this.saveButton = document.getElementById('save-button');
+
+		cancelButton.addEventListener('click', () => {
+			octopus.toggleVisibility(adminPanel);
+			octopus.clearAdminFields();
+		});
+
+		nameInput.addEventListener('input', (e) => {
+			newName = e.target.value;
+		});
+		urlInput.addEventListener('input', (e) => {
+			newURL = e.target.value;
+		});
+		clicksInput.addEventListener('input', (e) => {
+			newClicks = e.target.value;
+		});
+
+		adminView.render();
+	},
+	render: () => {
+		adminButton.addEventListener('click', () => {
+			octopus.toggleVisibility(adminPanel);
+			octopus.changeAdminFields();
+		});	
+
+		this.saveButton.addEventListener('click', () => {
+			if(typeof newName !== 'undefined'){
+				octopus.changeCurrentCatName(newName);
+			}
+			if(typeof newURL !== 'undefined'){
+				octopus.changeCurrentCatURL(newURL);
+			}
+			if(typeof newClicks !== 'undefined' && newClicks > -1){
+				octopus.changeCurrentCatClicks(newClicks);
+			}
+			octopus.toggleVisibility(adminPanel);
+			octopus.clearAdminFields();
+			detailView.render();
+		});
 	}
 };
 
@@ -95,13 +180,20 @@ var listView = {
 
 		for(let i=0; i<cats.length; i++){
 			let cat = cats[i];
-			let catButton = document.createElement('button');	
+			let catButton = document.createElement('button');
+			let catAttr = document.createAttribute("id");
+			catAttr.value = cat.name;
 			catButton.textContent = cat.name;
+			catButton.setAttributeNode(catAttr);
 
 			catButton.addEventListener('click', ((cat) => {
 				return () => {
 					octopus.setCurrentCat(cat);
+					octopus.hideAdminPanel();
 					detailView.render();
+					newName = octopus.getCurrentCat().name;
+					newURL = octopus.getCurrentCat().url;
+					newClicks = octopus.getCurrentCat().clickCount;
 				};
 			})(cat));
 
